2b27baa16d931bfe8299e2c3a93b25a7724a1a4e
mrdapoyo <wiiuforever25@gmail.com>
2024-04-22 20:34:22 +0200

    Refactor URL shortening functionality in main.py and update index.html

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, render_template, redirect, url_for
+import json
 import string, random
 
 app = Flask(__name__)
 
-shortened_urls = {}
+shortened_urls = json.load(open('urls.json', 'r'))
 
 def create_url(lenght=6):
     letters = string.ascii_letters + string.digits
@@ -17,8 +18,11 @@ def index():
         short_url = create_url()
         while short_url in shortened_urls:
             short_url = create_url()
+        if long_url in shortened_urls:
+            print("This URL is already shortened")
         shortened_urls[short_url] = long_url
-        print(shortened_urls)
+        with open('urls.json', '+w') as file:
+            json.dump(shortened_urls, file)
         return render_template("index.html", short_url=short_url)
     else:
         return render_template('index.html')
@@ -27,7 +31,7 @@ def index():
 def redirect_url(short_url):
     long_url = shortened_urls.get(short_url)
     if long_url: 
-        return redirect("https://" + long_url)
+        return redirect("http://" + long_url)
     else:
         return render_template("index.html", not_found=True)
 
